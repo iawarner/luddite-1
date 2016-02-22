5ac1f3c48f24ce14416953a693af72573a6ab120
Marina <marinabecker1031@gmail.com>
2016-02-22 14:20:32 -0800

    updated gff parsing script

--- luddite/parsers/gff3.py
+++ luddite/parsers/gff3.py
@@ -1,25 +1,54 @@
 #!/usr/bin/python
 
+"""
+USAGE:
+
+gff_file = gff3('path/to/file')
+
+for data in gff_file.getRecord()
+    # do a thing
+"""
+
+
+class gffRecord:
+    def __init__(self, Seqid, Source, Type, Start, End, Score, Strand, Phase, Attributes):
+        self.seqid = Seqid # column 1
+        self.source = Source # column 2
+        self.type = Type # column 3
+        self.start = Start # column 4
+        self.end = End # column 5
+        self.score = Score # column 6
+        self.strand = Strand # column 7
+        self.phase = Phase # column 8
+        self.attributes = Attributes # column 9
+
+
 class gff3:
+    """
+    A class for parsing gff3 format files, getRecord returns a generator that generates gffRecord objects
+    """
 
-    """A class for parsing gff3 format files"""
+    def __init__ (self, path_to_file):
+        with open(path_to_file, 'r') as f :
+            self.f = f
 
-    def __init__ (self):
-        # column 1
-        self.seqid =
-        # column 2
-        self.source
-        # column 3
-        self.type
-        # column 4
-        self.start
-        # column 5
-        self.end
-        # column 6
-        self.score
-        # column 7
-        self.strand
-        # column 8
-        self.phase
-        # column 9
-        self.attributes
+    def getRecord(self) :
+
+        gff_regex = compile('^(\S+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)') # regex to parse columns of annotation file
+
+        for line in self.f :
+            gff_matches = gff_regex.search(line) # find regex matches within the gene file
+
+            yield GFFRecord(gff_matches.group(1), # Seqid
+                            gff_matches.group(2), # Source
+                            gff_matches.group(3), # Type
+                            gff_matches.group(4), # Start
+                            gff_matches.group(5), # End
+                            gff_matches.group(6), # Score
+                            gff_matches.group(7), # Strand
+                            gff_matches.group(8), # Phase
+                            gff_matches.group(9)) # Attributes
+
+#    def PacBio_Parser():
+        # do this later
+        # parse self.attributes for more specific items
