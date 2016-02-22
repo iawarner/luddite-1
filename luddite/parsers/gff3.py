78309871d1bc4af0adb74d1861d323c920d76641
Marina <marinabecker1031@gmail.com>
2016-02-22 12:12:31 -0800

    new file for gff3 parsing

---
+++ luddite/parsers/gff3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+class gff3:
+
+    """A class for parsing gff3 format files"""
+
+    def __init__ (self):
+        # column 1
+        self.seqid =
+        # column 2
+        self.source
+        # column 3
+        self.type
+        # column 4
+        self.start
+        # column 5
+        self.end
+        # column 6
+        self.score
+        # column 7
+        self.strand
+        # column 8
+        self.phase
+        # column 9
+        self.attributes
