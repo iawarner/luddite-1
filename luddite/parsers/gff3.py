9fd7a02a2ef3c5311297816d93097ec7d076797c
Marina <marinabecker1031@gmail.com>
2016-02-24 12:41:15 -0800

    edits to match style of other parsers

--- luddite/parsers/gff3.py
+++ luddite/parsers/gff3.py
@@ -1,52 +1,51 @@
 #!/usr/bin/python
 
-"""
-USAGE:
-
-gff_file = gff3('path/to/file')
-
-for data in gff_file.getRecord()
-    # do a thing
-"""
+from openany import openany
 
 gff_regex = compile('^(\S+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)') # regex to parse columns of annotation file
 
-class gffRecord:
-    def __init__(self, Seqid, Source, Type, Start, End, Score, Strand, Phase, Attributes):
-        self.seqid = Seqid # column 1
-        self.source = Source # column 2
-        self.type = Type # column 3
-        self.start = Start # column 4
-        self.end = End # column 5
-        self.score = Score # column 6
-        self.strand = Strand # column 7
-        self.phase = Phase # column 8
-        self.attributes = Attributes # column 9
-
-
-class gff3:
+class gff3(object):
     """
     A class for parsing gff3 format files, getRecord returns a generator that generates gffRecord objects
     """
 
-    def __init__ (self, path_to_file):
-        with open(path_to_file, 'r') as f :
-            self.f = f
+    def __init__ (self, path):
+		self.file = openany(path)
+		self.path = path
 
-    def getRecord(self) :
-        for line in self.f :
-            gff_matches = gff_regex.search(line) # find regex matches within the gene file
+	def __repr__(self):
+		return '{}'.format(self.file)
 
-            yield GFFRecord(gff_matches.group(1), # Seqid
-                            gff_matches.group(2), # Source
-                            gff_matches.group(3), # Type
-                            gff_matches.group(4), # Start
-                            gff_matches.group(5), # End
-                            gff_matches.group(6), # Score
-                            gff_matches.group(7), # Strand
-                            gff_matches.group(8), # Phase
-                            gff_matches.group(9)) # Attributes
+    def parse(inc):
+        gff_matches = gff_regex.search(line) # find regex matches within the gene file
+        seqid        = gff_matches.group(1), # Seqid
+        source       = gff_matches.group(2), # Source
+        feature_type = gff_matches.group(3), # Type
+        start        = gff_matches.group(4), # Start
+        end          = gff_matches.group(5), # End
+        score        = gff_matches.group(6), # Score
+        strand       = gff_matches.group(7), # Strand
+        phase        = gff_matches.group(8), # Phase
+        attributes   = gff_matches.group(9)) # Attributes
+
+        return {'seqid'      : seqid,
+                'source'     : source,
+                'type'       : feature_type,
+                'start'      : start,
+                'end'        : end,
+                'score'      : score,
+                'strand'     : strand,
+                'phase'      : phase,
+                'attributes' : attributes}
 
 #    def PacBio_Parser():
         # do this later
         # parse self.attributes for more specific items
+
+    if __name__ == '__main__':
+    	# gff3 = gff3('file_name') # for testing
+            # only will run things here if script is run directly
+            # will not run if called via import
+
+    for record in gff3.readline():
+    	print '{}'.format(record)
